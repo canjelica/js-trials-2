0d47b556ab6d2b6d7d2e492f3f369e90cf8f22ad
Cassandra Sutton <cassandrasutton@gmail.com>
2020-05-19 16:43:27 -0700

    completed first function

--- arrays.js
+++ arrays.js
@@ -3,7 +3,23 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  //Print each item in the list, followed by its index.
+
+//   The output should look like this:
+//   apple 0
+//   berry 1
+//   cherry 2
+
+// Arguments:
+//   items (list)
+
+// Returns:
+  // None//
+  for (const item of items) {
+    console.log(item, items.indexOf(item))
+    }
+      
+      
 }
 
 
