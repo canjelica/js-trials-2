84fb78d78b2a63087e28244a33ad9da800f5bf29
Cassandra Sutton <cassandrasutton@gmail.com>
2020-05-19 21:09:57 -0700

    finished arrays

--- arrays.js
+++ arrays.js
@@ -17,19 +17,46 @@ function printIndices(items) {
   // None//
   for (const item of items) {
     console.log(item, items.indexOf(item))
-    }
-      
-      
+    } 
 }
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  //Print a list of every other item in `items`
+
+  //   Start with index 0.
+
+  //   Arguments:
+  //       items (list)
+
+  const result = []
+
+  for (const i in items) {
+    if (i % 2 === 0){
+      result.push(items[i]);
+    }
+  }
+  console.log(result);
 }
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  // Print a list of the `n` smallest integers in `items`.
+
+  // Order the integers in descending order.
+
+  // You can assume that `n` will be less than the length of the list.
+
+  // Arguments:
+  //     items (list[int]): A list of integers
+  //     n (int): Desired length for the resulting list
+  // 
+
+  const sorteditems = items.sort((a,b) => a - b).slice(0,n);
+  sorteditems.reverse();
+
+  console.log(sorteditems);
+
 }
