c21409539f1f8052ed9d809483bacfb01a824597
Cassandra Sutton <cassandrasutton@gmail.com>
2020-05-19 21:46:15 -0700

    Completed functions file

--- functions.js
+++ functions.js
@@ -1,16 +1,40 @@
 "use strict";
 
 
-// 1. isHometown
+function isHometown(town){
+    const hometown = 'Townsville'
+    
+    if (town === 'Townsville'){
+        return true;
+    }   else {
+        return false
+    }
+}
 
-// Define your function here
+
+function getFullName(firstName, lastName){
+    const message = `Hello ${firstName} ${lastName}!`;
+    return message
+}
 
 
-// 2. getFullName
 
-// Define your function here
+function calculateTotal(basePrice, state, tax=0.05){
+    
+    const subtotal = basePrice * (1 + tax);
+    let fee = 0;
+    if (state === 'CA'){
+        fee = 0.03 * subtotal;
+    }else if (state === 'PA'){
+        fee = 2;
+    }else if (state === 'MA') {
+        if (basePrice <= 100)
+            fee = 1;
+        else {
+            fee = 3;
+        }
+    }
+    return subtotal + fee;
+    
 
-
-// 3. calculateTotal
-
-// Define your function here
+}
